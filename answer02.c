0595b169c36bcf7702c3672ad8b08829493e9736
Hao Chen <chen2073@ecegrid-thin5.ecn.purdue.edu>
2017-06-23 16:52:05 -0400

    pa02

--- answer02.c
+++ answer02.c
@@ -10,6 +10,22 @@
 void Find_maze_dimensions(FILE *fptr, int *nrow, int *ncol)
 {
    *nrow = *ncol = 0;
+
+	fseek(fptr, 0, SEEK_SET);// reach the end of file
+	
+	int f;
+
+	while(f != EOF){ 
+ 
+	f = fgetc(fptr);
+
+	if(f == '\n'){  // count the rows
+	(*nrow)++;}
+	}
+    
+	
+	int length = (int) ftell(fptr);	 // count the number of characters in the file
+	*ncol =  (length / (*nrow)) - 1; // total number of characters divided by rows
 }
 
 /* Count the number of locations that are PATH */
@@ -18,7 +34,19 @@ void Find_maze_dimensions(FILE *fptr, int *nrow, int *ncol)
 
 int Count_path_locations(FILE *fptr)
 {
-   return 0;
+	int count = 0;
+	fseek(fptr, 0, SEEK_SET);
+	char f = 0;
+
+	do{
+	f = fgetc(fptr);
+
+	if(f == ' ' || f == '.' || f == 's' || f == 'e'){ 
+	count++;
+	}
+	}while(f != EOF);
+
+   return count;
 }
 
 /* Return the type of location: WALL or PATH */
@@ -28,7 +56,31 @@ int Count_path_locations(FILE *fptr)
 /* you may assume that the maze is of the correct format */
 
 char Get_location_type(FILE *fptr, int row, int col) {
-   return 0;
+	
+	int nrow=0;
+	int ncol=0; 
+	int f;
+ 
+	fseek(fptr, 0, SEEK_SET) ;
+	for(nrow=0; nrow <= row; nrow++){
+
+		
+	
+		for(ncol=0; ncol <= col; ncol++){
+					
+		f = fgetc(fptr);	
+
+		}
+	}
+			
+			if(f == '.' || f == 's' || f == 'e'){
+				return PATH;
+			}
+
+			else{
+				return WALL;
+			}
+//return 0;
 }
  
 /* Given a filename, re-represent the maze in the file pointer fptr */
@@ -40,5 +92,24 @@ char Get_location_type(FILE *fptr, int row, int col) {
 
 int Represent_maze_in_one_line(char *filename, FILE *fptr)
 {
-   return 0;
+
+	FILE * fp = fopen(filename, "w");
+	int f ;
+
+	fseek(fptr, 0, SEEK_SET) ;
+
+	if (fp == NULL) {
+		fprintf(stderr, "Error opening file");
+		return -1;
+  	}	
+	
+	while ((f = fgetc(fptr)) != EOF) {
+		if (f != '\n'){
+			fputc(f, fp) ;
+		}
+	}
+
+	fclose(fp);
+
+	return 0;
 }
